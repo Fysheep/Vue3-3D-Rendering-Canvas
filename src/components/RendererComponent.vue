abd74e4b768e5fd54e88e99109877a9d138bbac4
Fysheep <121219045+Fysheep@users.noreply.github.com>
2025-01-20 12:28:36 +0100

    Displaying Text for Viewport (viewport working)

--- src/components/RendererComponent.vue
+++ src/components/RendererComponent.vue
@@ -1,18 +1,51 @@
 <script lang="ts">
 export default {
   props: ['data'],
+  data() {
+    return {
+      renderer: undefined as any,
+    }
+  },
   mounted() {
-    const renderer = document.getElementById('renderer') as any
-    const parent = renderer?.parentElement
-    
-    renderer.height = parent?.clientHeight
-    renderer.width = parent?.clientWidth
-  }
+    window.addEventListener("resize", this.drawFrame);
+    this.drawFrame()
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.drawFrame);
+  },
+  methods: {
+    drawFrame() {
+      this.calcViewPort();
+      this.drawCircle()
+    },
+    getView(): { width: number, height: number } {
+      const width = this.renderer.width;
+      const height = this.renderer.height;
+
+      return { width, height }
+    },
+    calcViewPort() {
+      this.renderer = document.getElementById('renderer') as any;
+      const parent = this.renderer?.parentElement
+
+      this.renderer.height = parent?.clientHeight
+      this.renderer.width = parent?.clientWidth
+    },
+    drawCircle() {
+      const context: CanvasRenderingContext2D = this.renderer.getContext("2d");
+      const viewport = this.getView();
+
+
+      context.font = "30px Arial";
+      context.fillStyle = "White"
+      context.fillText(`Current Viewport: ${viewport.width}; ${viewport.height}`, 20, 50);
+    }
+  },
 }
 </script>
 
 <template>
-  <div class="expand fgr-1">
-    <canvas id="renderer" height="100%" width="100%" />
+  <div class="expand fgr-1 fsh-1">
+    <canvas id="renderer" height="100%" width="100%"> Your Browser does not Support Canvas</canvas>
   </div>
 </template>
