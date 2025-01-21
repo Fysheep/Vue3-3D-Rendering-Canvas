924af5213cd35568446adbcf9c0df0bc62660b06
Daniel Kunesch <121219045+Fysheep@users.noreply.github.com>
2025-01-21 17:52:22 +0100

    Added Display Stroke

--- src/components/RendererComponent.vue
+++ src/components/RendererComponent.vue
@@ -3,44 +3,53 @@ export default {
   props: ['data'],
   data() {
     return {
-      renderer: undefined as any,
+      renderer: undefined as any
     }
   },
   mounted() {
-    window.addEventListener("resize", this.drawFrame);
+    window.addEventListener('resize', this.drawFrame)
     this.drawFrame()
   },
   beforeUnmount() {
-    window.removeEventListener("resize", this.drawFrame);
+    window.removeEventListener('resize', this.drawFrame)
   },
   methods: {
     drawFrame() {
-      this.calcViewPort();
+      this.calcViewPort()
       this.drawCircle()
     },
-    getView(): { width: number, height: number } {
-      const width = this.renderer.width;
-      const height = this.renderer.height;
+    getView(): { width: number; height: number } {
+      const width = this.renderer.width
+      const height = this.renderer.height
 
       return { width, height }
     },
     calcViewPort() {
-      this.renderer = document.getElementById('renderer') as any;
+      this.renderer = document.getElementById('renderer') as any
       const parent = this.renderer?.parentElement
 
       this.renderer.height = parent?.clientHeight
       this.renderer.width = parent?.clientWidth
     },
     drawCircle() {
-      const context: CanvasRenderingContext2D = this.renderer.getContext("2d");
-      const viewport = this.getView();
+      const context: CanvasRenderingContext2D = this.renderer.getContext('2d')
+      const viewport = this.getView()
 
+      context.moveTo(0, 0)
+      context.lineWidth = Math.min(viewport.height, viewport.width) / 8
+      context.lineTo(viewport.width, viewport.height)
+      context.stroke()
 
-      context.font = "30px Arial";
-      context.fillStyle = "White"
-      context.fillText(`Current Viewport: ${viewport.width}; ${viewport.height}`, 20, 50);
+      context.moveTo(0, viewport.height)
+      context.lineWidth = Math.min(viewport.height, viewport.width) / 8
+      context.lineTo(viewport.width, 0)
+      context.stroke()
+
+      context.font = '30px Arial'
+      context.fillStyle = 'White'
+      context.fillText(`Current Viewport: ${viewport.width}; ${viewport.height}`, 20, 50)
     }
-  },
+  }
 }
 </script>
 
